eb36852af588a720747ea7de5b71e8b9eef1a640
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-06-23 23:53:32 -0300

    dynamic size to `input-generator`

--- input-generator.py
+++ input-generator.py
@@ -2,7 +2,11 @@ from faker import Faker
 
 fake = Faker()
 
+n = input();
+if n != '\n':
+    n = 50;
+
 with open("entrada.dat", "w+", encoding='utf-8') as f:
-    for i in range(50):
+    for i in range(n):
         f.write(fake.text(max_nb_chars=20) + ",")    # key
         f.write(fake.text(max_nb_chars=50, ext_word_list=None) + "\n")   # content
